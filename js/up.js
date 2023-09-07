ffa324c8bf20a58dc5ec482e041767d647162642
EminatarenX <eminataren2002@gmail.com>
2023-09-07 17:49:55 -0600

    comit

---
+++ js/up.js
@@ -0,0 +1,31 @@
+let slideIndex = 1;
+
+function cambiarSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+
+  if (n > slides.length) { slideIndex = 1 }    
+  if (n < 1) { slideIndex = slides.length }
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+
+  slides[slideIndex - 1].style.display = "block";  
+}
+
+function iniciarCarrusel() {
+    showSlides(slideIndex);
+    setInterval(function () {
+      cambiarSlide(1);
+    }, 25000); 
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  iniciarCarrusel();
+});
